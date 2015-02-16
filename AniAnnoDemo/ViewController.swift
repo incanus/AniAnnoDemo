b41a017f51d4e0cddf88bb6fac90356c66f93598
Justin R. Miller <incanus@codesorcery.net>
2015-02-16 15:04:34 -0800

    add connecting shape; reformat JSON

--- AniAnnoDemo/ViewController.swift
+++ AniAnnoDemo/ViewController.swift
@@ -14,19 +14,44 @@ class ViewController: UIViewController, MKMapViewDelegate {
 
         if let path = NSBundle.mainBundle().pathForResource("features", ofType: "json") {
             if let data = NSData(contentsOfFile: path) {
-                if let features = NSJSONSerialization.JSONObjectWithData(data, options: nil, error: nil) as? NSDictionary {
+                if let features = NSJSONSerialization.JSONObjectWithData(data,
+                    options: nil, error: nil) as? NSDictionary {
+
+                    var shapeCoordinates: [CLLocationCoordinate2D] = []
+
                     for feature: NSDictionary in features["features"] as Array {
-                        if let coordinates = (feature["geometry"] as NSDictionary)["coordinates"] as? Array<Double> {
+                        if let pointCoordinates = (feature["geometry"] as NSDictionary)["coordinates"] as? Array<Double> {
                             let annotation = MKPointAnnotation()
-                            annotation.coordinate = CLLocationCoordinate2D(latitude: coordinates[1], longitude: coordinates[0])
+                            annotation.coordinate = CLLocationCoordinate2D(latitude: pointCoordinates[1],
+                                longitude: pointCoordinates[0])
                             annotation.title = "Point \(map.annotations.count + 1)"
                             map.addAnnotation(annotation)
+
+                            shapeCoordinates.append(annotation.coordinate)
                         }
                     }
+
+                    let shape = MKPolygon(coordinates: &shapeCoordinates, count: shapeCoordinates.count)
+                    map.addOverlay(shape)
+
                     map.showAnnotations(map.annotations, animated: false)
                 }
             }
         }
+    }
+
+    func mapView(mapView: MKMapView!, rendererForOverlay overlay: MKOverlay!) -> MKOverlayRenderer! {
+        if overlay.isKindOfClass(MKPolygon) {
+            let renderer = MKPolygonRenderer(overlay: overlay)
+            renderer.fillColor = UIColor.orangeColor().colorWithAlphaComponent(0.5)
+            renderer.strokeColor = UIColor.blackColor()
+            renderer.lineDashPattern = [2, 4]
+            renderer.lineWidth = 2
+
+            return renderer
+        }
+
+        return nil
     }
 
 }
